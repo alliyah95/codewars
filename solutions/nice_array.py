aaae1e73756c277b944851fda67a693b58fc0076
allliyah <74038500+allliyah@users.noreply.github.com>
2021-10-12 01:04:58 +0800

    Create nice_array.py

---
+++ solutions/nice_array.py
@@ -0,0 +1,17 @@
+"""
+A Nice array is defined to be an array where for every value n in the array, there is also an element n-1 or n+1 in the array.
+
+example:
+
+[2,10,9,3] is Nice array because
+
+2=3-1
+10=9+1
+3=2+1
+9=10-1
+
+Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. You should also return false if input array has no elements.
+"""
+
+def is_nice(arr):
+    return False if len(arr) == 0 else len(arr) == len([x for x in arr if x+1 in arr or x-1 in arr])
